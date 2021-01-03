8b47f93863cd9d8bd7e50f94c08a32f609c48068
ソラノアナ Soranoana <Inanis_foramen@yahoo.co.jp>
2021-01-04 06:52:41 +0900

    発表用変更

--- Assets/Chutrial0.cs
+++ Assets/Chutrial0.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 //三角1面作製
 public class Chutrial0 : MonoBehaviour {
 	//頂点座標
-	Vector3[] TriangleVertex = new Vector3[3];
+	Vector3[] Vertex = new Vector3[3];
 
-	int[] TriangleFace = new int[3] { 0, 1, 2 };
+	//面情報
+	int[] Face = new int[3] { 0, 1, 2 };
 
 	//マテリアル
 	public Material material;
 
 	void Start() {
 		DisplayObject();
+		DisplayCaption("三角ポリゴン");
 	}
 
-	//直方体を表示する
+	//三角ポリゴン表示
 	private void DisplayObject() {
 		//必要なものをアタッチ
-		MeshFilter mesh_filter = this.gameObject.AddComponent<MeshFilter>();
-		MeshRenderer meshRenderer = this.gameObject.AddComponent<MeshRenderer>();
+		MeshFilter meshFilter
+			= this.gameObject.AddComponent<MeshFilter>();
+		MeshRenderer meshRenderer
+			= this.gameObject.AddComponent<MeshRenderer>();
 
 		//頂点計算
 		CalcVertices();
@@ -30,25 +34,41 @@ public class Chutrial0 : MonoBehaviour {
 		//合成用メッシュインスタンスのメッシュ領域を確保する
 		combineInstanceAry[0].mesh = new Mesh();
 		//頂点情報を追加
-		combineInstanceAry[0].mesh.vertices = TriangleVertex;
+		combineInstanceAry[0].mesh.vertices = Vertex;
 		//面情報を追加
-		combineInstanceAry[0].mesh.triangles = TriangleFace;
+		combineInstanceAry[0].mesh.triangles = Face;
 		//おまじない
 		combineInstanceAry[0].transform = Matrix4x4.Translate(Vector3.zero);
 
 		//メッシュの入れ先を確保する
-		mesh_filter.mesh = new Mesh();
+		meshFilter.mesh = new Mesh();
 		//メッシュのアタッチと合成
-		mesh_filter.mesh.CombineMeshes(combineInstanceAry);
+		meshFilter.mesh.CombineMeshes(combineInstanceAry);
 
+		//NormalMapの再計算
+		meshFilter.mesh.RecalculateNormals();
+		// meshFilter.mesh.normals = new Vector3[3] { new Vector3(0, 0, -1),
+		// 											new Vector3(0, 0, -1),
+		// 											new Vector3(0, 0, -1) };
 		//マテリアルアタッチ
 		meshRenderer.material = material;
 	}
 
 	//頂点計算
 	private void CalcVertices() {
-		TriangleVertex[0] = new Vector3(0, 0, 0);
-		TriangleVertex[1] = new Vector3(0, 1, 0);
-		TriangleVertex[2] = new Vector3(1, 0, 0);
+		//基点座標
+		Vertex[0] = new Vector3(0, 0, 0);
+		Vertex[1] = new Vector3(0, 1, 0);
+		Vertex[2] = new Vector3(1, 0, 0);
+	}
+
+	//キャプション表示
+	private void DisplayCaption(string caption) {
+		GameObject captionObj = new GameObject();
+		captionObj.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+		TextMesh textMesh = captionObj.AddComponent<TextMesh>();
+
+		textMesh.fontSize = 200;
+		textMesh.text = caption;
 	}
 }
--- Assets/Chutrial3.cs
+++ Assets/Chutrial3.cs
@@ -5,147 +5,73 @@ using UnityEngine;
 //四角（三角2面）作製（折り返しありフェイス分けなし）
 public class Chutrial3 : MonoBehaviour {
 	//頂点座標
-	Vector3[] StartVertex = new Vector3[4];
-	Vector3[] EndVertex = new Vector3[4];
-	Vector3[] CubeVertex = new Vector3[24];
-	//色の変更など
-	MeshRenderer meshRenderer;
-	//メッシュフィルター
-	MeshFilter mesh_filter;
+	Vector3[] Vertex = new Vector3[6];
 
 	//面情報
-	int[] Face = new int[36]{0,  1,  3,
-							0,  3,  2,
-
-							2 + 8,  3 + 8,  5,
-							2 + 8,  5,  4,
-
-							4 + 8,  5 + 8,  7,
-							4 + 8,  7,  6,
-
-							1 + 8,  6 + 8,  7 + 8,
-							1 + 8,  0 + 8,  6 + 8,
-
-							0 + 16,  2 + 16,  4 + 16,
-							0 + 16,  4 + 16,  6 + 16,
-
-							5 + 16,  3 + 16,  7 + 16,
-							3 + 16,  1 + 16,  7 + 16};
-
-	int[] EndFace = new int[6]  {1, 0,  3,
-								0,  2,  3 };
-	int[] SideFace = new int[24] { 2,      5,      4,
-									2,      3,      5,
-
-									1,      6,      7,
-									1,      0,      6,
-
-									2 + 8,  4 + 8,  0 + 8,
-									4 + 8,  6 + 8,  0 + 8,
-
-									7 + 8,  3 + 8,  1 + 8,
-									5 + 8,  3 + 8,  7 + 8,
-	};
+	int[] Face = new int[12] {  0, 1, 2,
+								1, 3, 2,
+								2, 3, 5,
+								2, 5, 4, };
 
 	//マテリアル
 	public Material material;
-	CombineInstance[] combineInstanceAry;
-
-	//生成したキューブを格納
-	private GameObject generetedCube;
 
 	void Start() {
 		DisplayObject();
+		DisplayCaption("折り返し");
 	}
 
-	//直方体を表示する
+	//折り返して表示
 	private void DisplayObject() {
-		//キューブ用空オブジェクト生成
-		generetedCube = new GameObject("myCube");
-		generetedCube.transform.position = new Vector3(0, 0, 0);
 		//必要なものをアタッチ
-		mesh_filter = this.generetedCube.AddComponent<MeshFilter>();
-		meshRenderer = this.generetedCube.AddComponent<MeshRenderer>();
-
-
-		// CombineMeshes()する時に使う配列   始端と終端も含めるので+3
-		combineInstanceAry = new CombineInstance[1];
+		MeshFilter meshFilter
+			= this.gameObject.AddComponent<MeshFilter>();
+		MeshRenderer meshRenderer
+			= this.gameObject.AddComponent<MeshRenderer>();
 
 		//頂点計算
 		CalcVertices();
 
-		Mesh mesh = new Mesh();
-		//メッシュリセット
-		mesh.Clear();
-		//メッシュへの頂点情報の追加
-		mesh.vertices = CubeVertex;
-		//メッシュへの面情報の追加
-		mesh.triangles = Face;
-		/*//メッシュへの頂点情報の追加
-		mesh.vertices = EndVertex;
-		//メッシュへの面情報の追加
-		mesh.triangles = EndFace;*/
-
-		// 合成するMesh（同じMeshを円形に並べたMesh）
-		// combineInstanceAry[0].mesh = meshLast;
-		combineInstanceAry[0].mesh = mesh;
+		//合成用インスタンスの配列
+		CombineInstance[] combineInstanceAry = new CombineInstance[1];
+		//合成用メッシュインスタンスのメッシュ領域を確保する
+		combineInstanceAry[0].mesh = new Mesh();
+		//頂点情報を追加
+		combineInstanceAry[0].mesh.vertices = Vertex;
+		//面情報を追加
+		combineInstanceAry[0].mesh.triangles = Face;
+		//おまじない
 		combineInstanceAry[0].transform = Matrix4x4.Translate(Vector3.zero);
-		//合成した（する）メッシュ
-		Mesh combinedMesh = new Mesh();
-		combinedMesh.name = transform.name;
-		combinedMesh.CombineMeshes(combineInstanceAry);
 
-		//メッシュアタッチ
-		mesh_filter.mesh = combinedMesh;
-		//レンダラーにマテリアルアタッチ
-		meshRenderer.material = material;
+		//メッシュの入れ先を確保する
+		meshFilter.mesh = new Mesh();
+		//メッシュのアタッチと合成
+		meshFilter.mesh.CombineMeshes(combineInstanceAry);
 
 		//NormalMapの再計算
-		mesh_filter.mesh.RecalculateNormals();
-		//できたら子供に。
-		generetedCube.transform.parent = this.gameObject.transform;
+		meshFilter.mesh.RecalculateNormals();
+		//マテリアルアタッチ
+		meshRenderer.material = material;
 	}
 
-	//直方体の素の頂点計算
+	//頂点計算
 	private void CalcVertices() {
-		//上側手前左の頂点座標
-		Vector3 vertex1 = new Vector3(0, 1, 0);
-		//上側手前右の頂点座標
-		Vector3 vertex2 = new Vector3(1, 1, 0);
-		//下側手前左の頂点座標
-		Vector3 vertex3 = new Vector3(0, 0, 0);
-		//下側手前右の頂点座標
-		Vector3 vertex4 = new Vector3(1, 0, 0);
-		//全頂点数8にそれぞれ座標が2つずつある
-		for (int i = 0; i < 8 * 3; i++) {
-			if (i % 8 == 0) {
-				CubeVertex[i] = vertex3;
-			} else if (i % 8 == 1) {
-				CubeVertex[i] = new Vector3(vertex3.x, vertex3.y, vertex3.z + 1);
-			} else if (i % 8 == 2) {
-				CubeVertex[i] = vertex1;
-			} else if (i % 8 == 3) {
-				CubeVertex[i] = new Vector3(vertex1.x, vertex1.y, vertex1.z + 1);
-			} else if (i % 8 == 4) {
-				CubeVertex[i] = vertex2;
-			} else if (i % 8 == 5) {
-				CubeVertex[i] = new Vector3(vertex2.x, vertex2.y, vertex2.z + 1);
-			} else if (i % 8 == 6) {
-				CubeVertex[i] = vertex4;
-			} else if (i % 8 == 7) {
-				CubeVertex[i] = new Vector3(vertex4.x, vertex4.y, vertex4.z + 1);
-			}
-		}
-		/*
-				//最初の端面
-				StartVertex[0] = vertex1;
-				StartVertex[1] = new Vector3(vertex1.x, vertex1.y, vertex1.z);
-				StartVertex[2] = vertex3;
-				StartVertex[3] = new Vector3(vertex3.x, vertex3.y, vertex3.z);
-				//最後の端面
-				EndVertex[0] = new Vector3(vertex2.x, vertex2.y, vertex2.z);
-				EndVertex[1] = vertex2;
-				EndVertex[2] = new Vector3(vertex4.x, vertex4.y, vertex4.z);
-				EndVertex[3] = vertex4;*/
+		//基点座標
+		Vertex[0] = new Vector3(0, 0, 0);
+		Vertex[1] = new Vector3(0, 1, 0);
+		Vertex[2] = new Vector3(1, 0, 0);
+		Vertex[3] = new Vector3(1, 1, 0);
+		Vertex[4] = new Vector3(1, 0, 1);
+		Vertex[5] = new Vector3(1, 1, 1);
+	}
+
+	//キャプション表示
+	private void DisplayCaption(string caption) {
+		GameObject captionObj = new GameObject();
+		captionObj.transform.localScale = new Vector3(0.035f, 0.035f, 0.035f);
+		TextMesh textMesh = captionObj.AddComponent<TextMesh>();
+
+		textMesh.fontSize = 200;
+		textMesh.text = caption;
 	}
 }
